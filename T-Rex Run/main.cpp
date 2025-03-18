582ddf039b5fa4d68191f8b39adc04abb6ffaf24
Hieu-PhamXuan <24021479@vnu.edu.vn>
2025-03-18 22:45:54 +0700

    Update main.cpp

--- T-Rex Run/main.cpp
+++ T-Rex Run/main.cpp
@@ -37,7 +37,7 @@ SDL_Texture* loadTexture(const char* path, SDL_Renderer* renderer) {
     return texture;
 }
 
-void close(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* background, SDL_Texture* sonicJumpFrames[], SDL_Texture* sonicRun[], SDL_Texture* obstacles[]) {
+void close(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* background, SDL_Texture* sonicJumpFrames[], SDL_Texture* sonicRun[], SDL_Texture* obstacles[], SDL_Texture* gameOverTexture) {
     SDL_DestroyTexture(background);
     for (int i = 0; i < 7; i++) {
         SDL_DestroyTexture(sonicJumpFrames[i]);
@@ -48,6 +48,7 @@ void close(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* background, 
     for (int i = 0; i < 3; i++) {
         SDL_DestroyTexture(obstacles[i]);
     }
+    SDL_DestroyTexture(gameOverTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -69,8 +70,9 @@ int main(int argc, char* argv[]) {
     if (!init(window, renderer)) return -1;
 
     SDL_Texture* background = loadTexture("D:\\Sonic\\background0.png", renderer);
-    if (!background) {
-        close(window, renderer, background, nullptr, nullptr, nullptr);
+    SDL_Texture* gameOverTexture = loadTexture("D:\\Sonic\\gameover.png", renderer);
+    if (!background || !gameOverTexture) {
+        close(window, renderer, background, nullptr, nullptr, nullptr, gameOverTexture);
         return -1;
     }
 
@@ -79,7 +81,7 @@ int main(int argc, char* argv[]) {
         string path = "D:/Sonic/sn" + to_string(i + 1) + ".png";
         sonicJumpFrames[i] = loadTexture(path.c_str(), renderer);
         if (!sonicJumpFrames[i]) {
-            close(window, renderer, background, sonicJumpFrames, nullptr, nullptr);
+            close(window, renderer, background, sonicJumpFrames, nullptr, nullptr, gameOverTexture);
             return -1;
         }
     }
@@ -89,7 +91,7 @@ int main(int argc, char* argv[]) {
         string path = "D:/Sonic/sonic_run" + to_string(i + 1) + ".png";
         sonicRun[i] = loadTexture(path.c_str(), renderer);
         if (!sonicRun[i]) {
-            close(window, renderer, background, sonicJumpFrames, sonicRun, nullptr);
+            close(window, renderer, background, sonicJumpFrames, sonicRun, nullptr, gameOverTexture);
             return -1;
         }
     }
@@ -104,12 +106,14 @@ int main(int argc, char* argv[]) {
 
     SDL_Rect sonicPos = {50, GROUND_Y, 100, 100};
 
-    SDL_Rect obstaclePos = {SCREEN_WIDTH, GROUND_Y + 30, 60, 60};
+    SDL_Rect obstaclePos = {SCREEN_WIDTH, GROUND_Y + 20, 60, 60};
     SDL_Texture* currentObstacle = obstacles[rand() % 3];
+    SDL_Rect gameOverRect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
 
     int obstacleGap = 20 + rand() % 50;
 
     bool quit = false;
+    bool isGameOver = false;
     SDL_Event e;
 
     int velocityY = 0;
@@ -131,6 +135,22 @@ int main(int argc, char* argv[]) {
                 jumpAnimationDone = false;
             }
         }
+        if (!isGameOver) {
+            if (obstaclePos.x < -50) {
+                obstaclePos.x = SCREEN_WIDTH + obstacleGap;
+                currentObstacle = obstacles[rand() % 3];
+            }
+
+            if (checkCollision(sonicPos, obstaclePos)) {
+                isGameOver = true;
+            }
+        }
+        if (isGameOver) {
+                SDL_RenderCopy(renderer, gameOverTexture, NULL, &gameOverRect);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(2000);
+                quit = true;
+            }
 
         if (isJumping) {
             sonicPos.y += velocityY;
@@ -164,9 +184,9 @@ int main(int argc, char* argv[]) {
         }
 
         if (checkCollision(sonicPos, obstaclePos)) {
-            cout << "Game Over!" << endl;
-            quit = true;
-        }
+                isGameOver = true;
+            }
+
 
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, background, NULL, &backgroundPos1);
@@ -175,10 +195,14 @@ int main(int argc, char* argv[]) {
         SDL_RenderCopy(renderer, currentObstacle, NULL, &obstaclePos);
         SDL_RenderPresent(renderer);
 
+        if (isGameOver) {
+            SDL_RenderCopy(renderer, gameOverTexture, NULL, &gameOverRect);
+        }
+
         SDL_Delay(16);
     }
 
-    close(window, renderer, background, sonicJumpFrames, sonicRun, obstacles);
+    close(window, renderer, background, sonicJumpFrames, sonicRun, obstacles, gameOverTexture);
 
     return 0;
 }
